a6480f1e89743a4b602b1854fa52341cb4aa31ae
ibramaf10 <94913110+ibramaf10@users.noreply.github.com>
2024-05-12 00:40:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
+    st.set_page_config(page_title="FinWise",
+                       page_icon="🏦")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
